d361cabc4f9256c93d6a9c1804bef41a2524492f
Robinnsson_Cifuentes_Ramirez <robinnsson@gmail.com>
2023-07-10 21:07:08 -0500

    Start project

---
+++ app/src/main/java/com/c2r/movies/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.c2r.movies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c2r.movies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
