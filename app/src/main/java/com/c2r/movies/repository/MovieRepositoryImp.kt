569afb262ea5ebdfde61dbefeea228f8bd32c24c
Robinnsson_Cifuentes_Ramirez <robinnsson@gmail.com>
2023-07-17 01:13:31 -0500

    Refactorización funcion de extensión .mapTo a genérica

--- app/src/main/java/com/c2r/movies/repository/MovieRepositoryImp.kt
+++ app/src/main/java/com/c2r/movies/repository/MovieRepositoryImp.kt
@@ -3,25 +3,25 @@ package com.c2r.movies.repository
 import com.c2r.movies.data.model.MovieUIList
 import com.c2r.movies.data.model.mapper.MovieMapper
 import com.c2r.movies.data.remote.MovieDataSource
-import com.c2r.movies.utils.mapToUI
+import com.c2r.movies.utils.mapTo
 
 
 class MovieRepositoryImp(private val movieDataSource: MovieDataSource) : MovieRepository {
 
     override suspend fun getUpComingMovies(): MovieUIList {
         val movieApiList = movieDataSource.getUpComingMovies().results
-        val movieList = movieApiList.mapToUI { movieApi -> MovieMapper.mapToUI(movieApi) }
+        val movieList = movieApiList.mapTo { movieApi -> MovieMapper.mapToUI(movieApi) }
         return MovieUIList(movieList)
     }
     override suspend fun getTopRatedMovies(): MovieUIList {
         val movieApiList = movieDataSource.getTopRatedMovies().results
-        val movieList = movieApiList.mapToUI { movieApi -> MovieMapper.mapToUI(movieApi) }
+        val movieList = movieApiList.mapTo { movieApi -> MovieMapper.mapToUI(movieApi) }
         return MovieUIList(movieList)
     }
 
     override suspend fun getPopularMovies(): MovieUIList {
         val movieApiList = movieDataSource.getPopularMovies().results
-        val movieList = movieApiList.mapToUI { movieApi -> MovieMapper.mapToUI(movieApi) }
+        val movieList = movieApiList.mapTo { movieApi -> MovieMapper.mapToUI(movieApi) }
         return MovieUIList(movieList)
     }
 
--- app/src/main/java/com/c2r/movies/utils/Extention.kt
+++ app/src/main/java/com/c2r/movies/utils/Extention.kt
@@ -5,6 +5,6 @@ import com.c2r.movies.data.model.MovieUI
 import com.c2r.movies.data.model.MovieUIList
 import com.c2r.movies.data.model.mapper.MovieMapper
 
-fun List<MovieApi>.mapToUI(mapper: (MovieApi) -> MovieUI): List<MovieUI> {
-    return this.map { movie -> mapper(movie) }
+fun <A, B>List<A>.mapTo(mapper: (A) -> B): List<B> {
+    return this.map { item -> mapper(item) }
 }
